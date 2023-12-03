16410e05833771da79a32bc6ffde22cdf8000cea
elisiosantos <elisio13cantil@gmail.com>
2023-12-03 15:36:31 -0300

    Filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -35,6 +35,22 @@ app.post('/completar',(requisicao, resposta)=>{
     })
 })
 
+app.post('/descompletar', (requisicao, resposta)=>{
+    const id = requisicao.body.id
+
+    const sql = `
+    UPDATE TAREFAS
+    SET completa = '0'
+    WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) =>{
+        return console.log(erro)
+    })
+
+    resposta.redirect('/')
+})
+
 app.post('/criar', (requisicao, resposta) =>{
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -68,7 +84,14 @@ app.get('/', (requisicao, resposta) =>{
                 completa: dado.completa === 0 ? false : true
             }
         })
-        resposta.render('home', {tarefas})
+
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefasAtivas = tarefasAtivas.length
+
+        resposta.render('home', {tarefas, quantidadeTarefasAtivas})
     })
 })
 
